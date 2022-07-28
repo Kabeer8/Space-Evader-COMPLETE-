b0dfb7ade36a5680808b63fedf7ebf62218d46c7
Kabeer <K.sah@share.epsb.ca>
2022-07-27 18:07:27 -0600

    commit

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Creates Canvas
-
 let cnv = document.getElementById("my-canvas");
 let ctx = cnv.getContext("2d");
 cnv.height = 620;
@@ -8,16 +7,16 @@ let animateIndex = 1;
 let playerHP = 100;
 let villainHP = 100;
 let fire = true;
-
+let go = false;
 
 //Creates player 
 let goodGuy = new Image();
-goodGuy.src = "img/Protaginist Ship Frames.png";
-let player = {x:800, y:310, yVU:0, yVD:0, w:70, h:20, yF:0};
+goodGuy.src = "assets/Protaginist Ship Frames.png";
+let player = {x:800, y:310, yVU:0, yVD:0, w:80, h:30, yF:0};
 
 //Creates badGuy
 let badGuy = new Image();
-badGuy.src = "img/Bad Guy Frames.png";
+badGuy.src = "assets/Bad Guy Frames.png";
 let villain = {x:0, y:310, yV:0, w:400, h:250, yF:0};
 
 //Create laser and bullet
@@ -30,7 +29,7 @@ function laser(x1,y1,x2,y2,c){
     ctx.stroke();
 }
 let missile = new Image();
-missile.src = "img/Missile Frames.png";
+missile.src = "assets/Missile Frames.png";
 let bullet = {x: villain.x + 400, y: villain.y + 170, w: 0, h: 0, yF: 0};
 
 //Create Asteroids
@@ -41,9 +40,9 @@ asteroidsGen()
 
 function asteroidsGen(){
     asteroids.push(newAsteroid(Math.floor((Math.random() * 1000) + 1300),
-    Math.floor(Math.random() * 630),
+    Math.floor(Math.random() * 530),
     Math.floor((Math.random() * 3) + 10),
-    Math.floor((Math.random() * 2) + 5)));
+    (Math.random() * 3) + 6));
     }
 function newAsteroid(initX, initY, initR, initS){
     return{
@@ -57,7 +56,7 @@ function drawAsteroids(asteroid){
     x = asteroid.x
     y = asteroid.y
     r = asteroid.r
-    ctx.fillStyle = "brown"
+    ctx.fillStyle = "lightgrey"
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2*Math.PI);
     ctx.fill();
@@ -76,6 +75,8 @@ function rect(x,y,w,h,c){
     ctx.fillStyle = c;
     ctx.fillRect(x, y, w, h);
 }
+let badBar = "green"
+let goodBar = "green"
 
 //UI text
 function text(text, x, y,c){
@@ -83,6 +84,19 @@ function text(text, x, y,c){
     ctx.font = "20px Courier New";
     ctx.fillText(text, x, y);
 }
+
+// creates sounds effects
+
+let loss = new Audio("assets/Lose.mp3");
+let hit = new Audio ("assets/Hit.mp3");
+let missileSFX = new Audio ("assets/missile.mp3");
+let win = new Audio ("assets/Win.mp3");
+let boom = new Audio ("assets/Boom.mp3");
+let info = new Audio("assets/instructions.mp3")
+let music = new Audio("assets/ItHasToBeThisWay.mp3")
+missileSFX. volume = 0.4;
+music.volume = 0.5;
+
 
 let raf
 function animate(){
@@ -95,16 +109,10 @@ function animate(){
             asteroids[i].x = 1300;
             asteroids[i].y = Math.floor(Math.random() * 630);
          }
-         for(d = 0; d < 67; d++){
-            if(dist(player.x + d, player.y, asteroids[i].x , asteroids[i].y) <= 10){
+         for(d = 0; d < 80; d++){
+            if(dist(player.x + d, player.y + 15, asteroids[i].x , asteroids[i].y) <= 15){
+                hit.play()
                 playerHP -= 10;
-                asteroids[i].x = 1300;
-                asteroids[i].y = Math.floor(Math.random() * 630);
-            }
-        }
-        for(d = 0; d < 310; d++){
-            if(dist(villain.x, villain.y + d, asteroids[i].x, asteroids[i].y) <= 30){
-                villainHP -= 0.5;
                 asteroids[i].x = 1300;
                 asteroids[i].y = Math.floor(Math.random() * 630);
             }
@@ -170,36 +178,43 @@ function animate(){
                     y: villain.y + 170,
                     w: 60,
                     h: 10,
-                    c: "blue"
                 }
+                missileSFX.play();
             };
         }, 2000);
     }
     ctx.drawImage(missile, 0, bullet.yF, 986, 100, bullet.x, bullet.y, bullet.w, bullet.h);
     if(bullet.w == 60){
-        bullet.x += 10;
+        bullet.x += 8;
         fire = false;
     }
     if(fire == false){
-        if(bullet.y > player.y ){
-            bullet.y -= 2;
+        if(bullet.y > player.y + 5 ){
+            bullet.y -= 3;
         }
-        if(bullet.y < player.y ){
-            bullet.y += 2;
+        if(bullet.y < player.y + 5 ){
+            bullet.y += 3;
         }
-        if(bullet.x >= 2000){
+        if(bullet.x >= 2500){
+            missileSFX.pause();
+            missileSFX.currentTime = 0;
             bullet.x = villain.x + 400;
+            bullet.y = villain.y + 170
             fire = true;
         }
-        
+        if(bullet.y == villain.y + 170){
+            missileSFX.play()
+        }
     }
-    for(d = 0; d < 67; d++){
-        if(dist(player.x + d, player.y, bullet.x + d, bullet.y) <= 10){
+    for(d = 0; d < 80; d++){
+        if(dist(player.x + d, player.y + 15, bullet.x + d, bullet.y) <= 15){
+            missileSFX.pause();
+            missileSFX.currentTime = 0;
+            boom.play()
             playerHP -= 50;
             bullet.x = 1300;
         }
     }
-    
     //UI
     rect(0, 520,1300, 100, "DarkSlateGray");
     text("Destroyer", 250, 550, "white" );
@@ -208,21 +223,54 @@ function animate(){
     rect(750, 560, 500, 50, "black");
     if (playerHP <= 0){ playerHP = 0;}
     if (villainHP <= 0){villainHP = 0;}
-    rect(50, 560, villainHP*5, 50, "green");
-    rect(750, 560, playerHP*5, 50, "green");
-    text(villainHP +"%", 275, 580, "white" );
+    if (villainHP <= 40){badBar = "yellow";} 
+    if(villainHP <= 20){badBar = "red";}
+    if (playerHP <= 40){goodBar = "yellow";} 
+    if(playerHP <= 20){goodBar = "red";}
+    rect(50, 560, villainHP*5, 50, badBar);
+    rect(750, 560, playerHP*5, 50, goodBar);
+    text(Math.round(villainHP) +"%", 275, 580, "white" );
     text(playerHP +"%", 950, 580, "white" );
 
     if (playerHP <= 0){
         rect(0, 0, 1300, 520, "black");
         text("Objective failed, refresh to try again.", 425, 250, "red" );
+        music.pause();
+        music.currentTime = 0;
+        loss.play();
         cancelAnimationFrame(raf)
     }
     if (villainHP <= 0){
+        missileSFX.pause();
         rect(0, 0, 1300, 520, "black");
         text("Objective complete, refresh to play again.", 425, 250, "green" );
-        cancelAnimationFrame(raf)
+        cancelAnimationFrame(raf);
     }
 }
 
-animate()
+text("Press space to start", 525, 250, "white" );
+text("Press 'i' for information", 500, 270, "white" );
+
+document.addEventListener("keyup", start)
+function start(event){
+    if(event.keyCode === 73){
+        info.play()
+        text("Your mission: Evade the missiles deployed by the destroyer while cruising through an ", 125, 300, "white" );
+        text("asteroid belt. The destroyer is slow and not suited to its current environment. Use ", 125, 320, "white" );
+        text("this to your advantage. Use the left arrow key to go up and the right one to go down.", 125, 340, "white" );
+        text("Once ready, you may press the space bar to start, good luck.", 250, 360,"white");
+    }
+    if(event.keyCode === 32){
+        if(go == false){
+            animate()
+            music.play();
+            setInterval(function(){
+                villainHP -= 1.7;
+            },1000);
+            go = true;
+        }
+        info.pause();
+        info.currentTime = 0;
+    }
+}
+
--- player.js
+++ player.js
@@ -1,24 +1,31 @@
 
-
+let paused = false;
 document.addEventListener("keydown", keydownHandler)
 function keydownHandler(event){
-
-    if(event.keyCode === 65){
-        player.yVU = -5;
+    if(event.keyCode === 37){
+        player.yVU = -6;
     }
-    if(event.keyCode === 68){
-        player.yVD = 5;
+    if(event.keyCode === 39){
+        player.yVD = 6;
     }
-
-
+    if(event.keyCode === 80){
+        if (paused == false){
+        cancelAnimationFrame(raf);
+        paused = true;
+        }
+        else{
+            animate();
+        paused = false;
+        }
+    }
 }
 document.addEventListener("keyup", keyupHandler)
 function keyupHandler(event){
 
-    if(event.keyCode === 65){
+    if(event.keyCode === 37){
         player.yVU = 0;
     }
-    if(event.keyCode === 68){
+    if(event.keyCode === 39){
         player.yVD = 0;
     }
 }
